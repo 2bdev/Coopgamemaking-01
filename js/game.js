6d05a5e5ea3c06ea4ba71652898d16f11779ad7d
Brian B <brian.borman@kingdesignllc.com>
2015-05-07 11:22:17 -0400

    Added avail stat points, added char div, added cur health vs max health, feature to only increase exp if health is full

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 /* Creating global Vars */
 
 // Stats 
-var health = 100;
+var max_health = 100;
+var cur_health = max_health;
 var strength = 1;
 var intellect = 1;
 var dexterity = 1;
@@ -18,11 +19,25 @@ var game_time = 250;
 var prev_time = new Date();
 var passed_time = 0;
 
+// Game Area
+var game_width;
+var game_height = 600;
+
+// Characters
+var char_width = 100;
+var char_height = 100;
+
+// Enemies
+var enemies = new Array();
+
 /* Updating variables on page when the document loads */
 $(document).ready(function(e) {
 	
 	// Initiate stats on screen
 	update_stats();
+
+	// Set game area size
+	init_game_area();
 	
 	// Game Loop
 	var game_tick = setInterval(function(){
@@ -52,10 +67,12 @@ var update_stats = function(){
 	
 	// Updating stats
 	$("#level").html(level);
-	$("#health").html(health);
+	$(".max_health").html(max_health);
+	$(".cur_health").html(cur_health);
 	$("#strength").html(strength);
 	$("#intellect").html(intellect);
 	$("#dexterity").html(dexterity);
+	$("#avail_stat_points").html(stat_points);
 	$("#exp").html(experience);
 	$("#exp_tnl").html(experience_tnl);
 	
@@ -63,16 +80,22 @@ var update_stats = function(){
 
 var update_game = function(passed_time){
 		
-	// Adds exp each game tick
-	experience += 10*passed_time;
-	if(experience > experience_tnl){
-		experience -= experience_tnl;
-		experience_tnl = get_experience_tnl();
-		level += 1;
-		stat_points += 2;
-		max_stat_points += 2;
+	// only add experience if health is full
+	// otherwise heal player until full health
+	if(cur_health < max_health) {
+		cur_health++;
+	} else {
+		// Adds exp each game tick
+		experience += 10*passed_time;
+		if(experience > experience_tnl){
+			experience -= experience_tnl;
+			experience_tnl = get_experience_tnl();
+			level += 1;
+			stat_points += 2;
+			max_stat_points += 2;
+		}
 	}
-	
+
 	// Updates stats on the screen
 	update_stats();
 	
@@ -103,12 +126,28 @@ var check_stat_points = function(){
 	
 }
 
+var init_game_area = function() {
+	// get and set game width
+	game_width = $("#game_area").width();
+	// set game area height
+	$("#game_area").height(game_height + "px");
+
+	$("#game_area").append('<div id="player"><span class="cur_health">'+cur_health+'</span>/<span class="max_health">'+max_health+'</span></div>');
+	$("#player").css("bottom", 20);
+	$("#player").css("left", (game_width / 2) - (char_width / 2));
+	
+}
+
+var spawn_enemy = function() {
+
+}
+
 /* Checks for clicks on stat upgrades */
 
-// Health
+// Max Health
 $("#health_add").click(function(){
 	if(can_level_stats){
-		health += 10;
+		max_health += 10;
 		stat_points -= 1;
 		check_stat_points();
 	}
